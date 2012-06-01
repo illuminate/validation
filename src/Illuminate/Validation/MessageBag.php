0fcdc957dba069c002eaff0ade20a3112d4f948f
Taylor Otwell <taylorotwell@gmail.com>
2012-05-31 23:44:46 -0500

    formatting.

--- src/Illuminate/Validation/MessageBag.php
+++ src/Illuminate/Validation/MessageBag.php
@@ -123,8 +123,8 @@ class MessageBag {
 	{
 		$messages = (array) $messages;
 
-		// We will simply spin through the given messages and transform each one,
-		// replace the :message place holder with the actual message allowing
+		// We will simply spin through the given messages and transform each one
+		// replacing the :message place holder with the real message allowing
 		// the messages to be easily formatted to each developer's liking.
 		foreach ($messages as $key => &$message)
 		{
