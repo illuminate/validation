bed87759d21cd4c657b423e97a5127ed54d9e6a0
Taylor Otwell <taylorotwell@gmail.com>
2012-05-31 23:12:44 -0500

    work on the message container.'

--- src/Illuminate/Validation/MessageContainer.php
+++ src/Illuminate/Validation/MessageContainer.php
@@ -3,18 +3,18 @@
 class MessageContainer {
 
 	/**
-	 * All of the messages.
+	 * All of the registered messages.
 	 *
 	 * @var array
 	 */
 	protected $messages;
 
 	/**
-	 * The global format of the messages.
+	 * Default format for message output.
 	 *
-	 * @return string
-	 */
-	protected $format;
+	 * @var string
+	 */	
+	protected $format = ':message';
 
 	/**
 	 * Create a new message container instance.
@@ -27,6 +27,167 @@ class MessageContainer {
 	{
 		$this->format = $format;
 		$this->messages = $messages;
+	}
+
+	/**
+	 * Add a message to the container.
+	 *
+	 * @param  string  $key
+	 * @param  string  $message
+	 * @return void
+	 */
+	public function add($key, $message)
+	{
+		if ($this->isUnique($key, $message))
+		{
+			$this->messages[$key][] = $message;
+		}
+	}
+
+	/**
+	 * Determine if a key and message combination already exists.
+	 *
+	 * @param  string  $key
+	 * @param  string  $message
+	 * @return bool
+	 */
+	protected function isUnique($key, $message)
+	{
+		$messages = $this->messages;
+
+		return ! isset($messages[$key]) or ! in_array($message, $messages[$key]);
+	}
+
+	/**
+	 * Determine if messages exist for a given key.
+	 *
+	 * @param  string  $key
+	 * @return bool
+	 */
+	public function has($key = null)
+	{
+		return $this->first($key) !== '';
+	}
+
+	/**
+	 * Get the first message from the container for a given key.
+	 *
+	 * @param  string  $key
+	 * @param  string  $format
+	 * @return string
+	 */
+	public function first($key = null, $format = null)
+	{
+		$format = $this->checkFormat($format);
+
+		$messages = is_null($key) ? $this->all($format) : $this->get($key, $format);
+
+		return (count($messages) > 0) ? $messages[0] : '';
+	}
+
+	/**
+	 * Get all of the messages from the container for a given key.
+	 *
+	 * @param  string  $key
+	 * @param  string  $format
+	 * @return array
+	 */
+	public function get($key, $format = null)
+	{
+		$format = $this->checkFormat($format);
+
+		// If the message exists in the container, we will transform it and return
+		// the message. Otherwise, we'll return an empty array since the whole
+		// method is to give back an array of messages in the first place.
+		if (array_key_exists($key, $this->messages))
+		{
+			return $this->transform($this->messages[$key], $format);
+		}
+
+		return array();
+	}
+
+	/**
+	 * Get all of the messages for every key in the container.
+	 *
+	 * @param  string  $format
+	 * @return array
+	 */
+	public function all($format = null)
+	{
+		$format = $this->checkFormat($format);
+
+		$all = array();
+
+		foreach ($this->messages as $messages)
+		{
+			$all = array_merge($all, $this->transform($messages, $format));
+		}
+
+		return $all;
+	}
+
+	/**
+	 * Format an array of messages.
+	 *
+	 * @param  array   $messages
+	 * @param  string  $format
+	 * @return array
+	 */
+	protected function transform($messages, $format)
+	{
+		$messages = (array) $messages;
+
+		// We will simply spin through the given messages and transform each one,
+		// replace the :message place holder with the actual message allowing
+		// the messages to be easily formatted to each developer's liking.
+		foreach ($messages as $key => &$message)
+		{
+			$message = str_replace(':message', $message, $format);
+		}
+
+		return $messages;
+	}
+
+	/**
+	 * Get the appropriate format based on the given format.
+	 *
+	 * @param  string  $format
+	 * @return string
+	 */
+	protected function checkFormat($format)
+	{
+		return ($format === null) ? $this->format : $format;
+	}
+
+	/**
+	 * Get the raw messages in the container.
+	 *
+	 * @return array
+	 */
+	public function getMessages()
+	{
+		return $this->messages;
+	}
+
+	/**
+	 * Get the default message format.
+	 *
+	 * @return string
+	 */
+	public function getFormat()
+	{
+		return $this->format;
+	}
+
+	/**
+	 * Set the default message format.
+	 *
+	 * @param  string  $format
+	 */
+	public function setFormat($format = ':message')
+	{
+		$this->format = $format;
 	}
 
 }
