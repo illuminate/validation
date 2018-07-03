151008dfe256786452f9efa25ed94b89804f307f
Ahmed Hulo <ahmedh@axis.com>
2018-07-03 15:34:56 +0200

    Fix validateInteger function
    Should strictly check that the type is an integer
    
    Change-Id: I4aa91e3c4fe6a462e1266ebe32e0ef27f97c4313

--- Concerns/ValidatesAttributes.php
+++ Concerns/ValidatesAttributes.php
@@ -993,7 +993,7 @@ trait ValidatesAttributes
      */
     public function validateInteger($attribute, $value)
     {
-        return filter_var($value, FILTER_VALIDATE_INT) !== false;
+        return is_int($value);
     }
 
     /**
